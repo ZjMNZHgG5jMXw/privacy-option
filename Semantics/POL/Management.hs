6ab9b1ee94b232a50f394436082dc90f3f9654e5
Stefan Berthold <stefan.berthold@gmx.net>
2013-10-31 18:51:43 +0100

    moved the management semantics of and to privacy-option-simpol.

--- Semantics/POL/Management.hs
+++ Semantics/POL/Management.hs
@@ -2,13 +2,13 @@
 module Semantics.POL.Management where
 import Prelude hiding ( and )
 import qualified Prelude as P
-import Control.Parallel
 import Language.POL.Syntax hiding ( when )
 import Data.POL.Observable ( ObservableT )
 class Monad m => Management a p l m where
   use         :: a -> p -> m (Contract a p l m)
   send        :: Contract a p l m -> m (Contract a p l m)
   greedy      :: Contract a p l m -> Contract a p l m -> m (Contract a p l m)
+  parallel    :: Contract a p l m -> Contract a p l m -> m (Contract a p l m)
   ifthenelse  :: ObservableT l m Bool -> Contract a p l m -> Contract a p l m -> m (Contract a p l m)
   when        :: ObservableT l m Bool -> Contract a p l m -> m (Contract a p l m)
   stopping    :: ObservableT l m Bool -> Contract a p l m -> m (Contract a p l m)
@@ -17,10 +17,7 @@ execute :: (Management a p l m, Ord a, Ord p, Ord l) => Contract a p l m -> m (C
 execute Zero          = return Zero
 execute (Data a p)    = use a p
 execute (Give c)      = send c
-execute (And c1 c2)   = do
-  c1' <- execute c2 `par` execute c1
-  c2' <- execute c2
-  return $ c1' `and` c2'
+execute (c1 `And` c2) = parallel c1 c2
 execute (c1 `Or` c2)  = greedy c1 c2
 execute (If o c)      = ifthenelse o c Zero
 execute (IfNot o c)   = ifthenelse o Zero c
