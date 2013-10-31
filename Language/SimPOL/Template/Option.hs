15a8bf967a38711512e554b04ae113854ec673e2
Stefan Berthold <stefan.berthold@gmx.net>
2013-10-31 11:25:57 +0100

    package split into privacy-option and privacy-option-simpol.

--- Language/SimPOL/Template/Option.hs
+++
@@ -1,6 +0,0 @@
-module Language.SimPOL.Template.Option where
-import Prelude hiding ( or )
-import Language.SimPOL
-option :: Contract -> Contract
-option = or zero
--- vim: ft=haskell:sts=2:sw=2:et:nu:ai
